65fd83e09b7bbec7c14a3852e8064377d5e6595f
M0rnstar <azamat.mambetov.2014@gmail.com>
2025-06-20 22:48:30 +0300

    #

--- csv_processor/__init__.py
+++ csv_processor/__init__.py
@@ -2,4 +2,4 @@ from .parser import parse_args
 from .csv_reader import read_csv
 from .filters import apply_filter
 from .aggregators import apply_aggregation
-from .display import print_table
+from .display import print_table, print_aggregation_table
